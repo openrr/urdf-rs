ab9aa34718db2aa70606a906586f0a862b1e90d5
Taiki Endo <te316e89@gmail.com>
2023-12-26 19:40:37 +0900

    Update yaserde from 0.8 to 0.9

--- src/deserialize.rs
+++ src/deserialize.rs
@@ -1,7 +1,6 @@
 use xml::attribute::OwnedAttribute;
 use xml::namespace::Namespace;
 use yaserde::{YaDeserialize, YaSerialize};
-use yaserde_derive::{YaDeserialize, YaSerialize};
 
 use std::io::{Read, Write};
 
